26d8246108a5836453b0fe30f5a4af6cf5e592e9
Sovathna Hong <hongsovathna@gmail.com>
2021-06-18 12:30:15 +0700

    update VLog

--- src/debug/java/com/sovathna/androidmvi/VLog.kt
+++ src/debug/java/com/sovathna/androidmvi/VLog.kt
@@ -8,63 +8,63 @@ object VLog : AbstractLogger() {
     Timber.plant(Timber.DebugTree())
   }
 
-  override fun d(throwable: Throwable) {
+  override fun d(throwable: Throwable?) {
     Timber.tag(TAG).d(throwable)
   }
 
-  override fun d(message: String) {
+  override fun d(message: String?) {
     Timber.tag(TAG).d(message)
   }
 
-  override fun d(throwable: Throwable, message: String) {
+  override fun d(throwable: Throwable?, message: String?) {
     Timber.tag(TAG).d(throwable, message)
   }
 
-  override fun e(throwable: Throwable) {
+  override fun e(throwable: Throwable?) {
     Timber.tag(TAG).e(throwable)
   }
 
-  override fun e(message: String) {
+  override fun e(message: String?) {
     Timber.tag(TAG).e(message)
   }
 
-  override fun e(throwable: Throwable, message: String) {
+  override fun e(throwable: Throwable?, message: String?) {
     Timber.tag(TAG).e(throwable, message)
   }
 
-  override fun i(throwable: Throwable) {
+  override fun i(throwable: Throwable?) {
     Timber.tag(TAG).i(throwable)
   }
 
-  override fun i(message: String) {
+  override fun i(message: String?) {
     Timber.tag(TAG).i(message)
   }
 
-  override fun i(throwable: Throwable, message: String) {
+  override fun i(throwable: Throwable?, message: String?) {
     Timber.tag(TAG).i(throwable, message)
   }
 
-  override fun v(throwable: Throwable) {
+  override fun v(throwable: Throwable?) {
     Timber.tag(TAG).v(throwable)
   }
 
-  override fun v(message: String) {
+  override fun v(message: String?) {
     Timber.tag(TAG).v(message)
   }
 
-  override fun v(throwable: Throwable, message: String) {
+  override fun v(throwable: Throwable?, message: String?) {
     Timber.tag(TAG).v(throwable, message)
   }
 
-  override fun wtf(throwable: Throwable) {
+  override fun wtf(throwable: Throwable?) {
     Timber.tag(TAG).wtf(throwable)
   }
 
-  override fun wtf(message: String) {
+  override fun wtf(message: String?) {
     Timber.tag(TAG).wtf(message)
   }
 
-  override fun wtf(throwable: Throwable, message: String) {
+  override fun wtf(throwable: Throwable?, message: String?) {
     Timber.tag(TAG).wtf(throwable, message)
   }
 }
--- src/main/java/com/sovathna/androidmvi/AbstractLogger.kt
+++ src/main/java/com/sovathna/androidmvi/AbstractLogger.kt
@@ -3,33 +3,33 @@ package com.sovathna.androidmvi
 abstract class AbstractLogger {
   open fun init() {}
 
-  open fun d(throwable: Throwable) {}
+  open fun d(throwable: Throwable?) {}
 
-  open fun d(message: String) {}
+  open fun d(message: String?) {}
 
-  open fun d(throwable: Throwable, message: String) {}
+  open fun d(throwable: Throwable?, message: String?) {}
 
-  open fun e(throwable: Throwable) {}
+  open fun e(throwable: Throwable?) {}
 
-  open fun e(message: String) {}
+  open fun e(message: String?) {}
 
-  open fun e(throwable: Throwable, message: String) {}
+  open fun e(throwable: Throwable?, message: String?) {}
 
-  open fun i(throwable: Throwable) {}
+  open fun i(throwable: Throwable?) {}
 
-  open fun i(message: String) {}
+  open fun i(message: String?) {}
 
-  open fun i(throwable: Throwable, message: String) {}
+  open fun i(throwable: Throwable?, message: String?) {}
 
-  open fun v(throwable: Throwable) {}
+  open fun v(throwable: Throwable?) {}
 
-  open fun v(message: String) {}
+  open fun v(message: String?) {}
 
-  open fun v(throwable: Throwable, message: String) {}
+  open fun v(throwable: Throwable?, message: String?) {}
 
-  open fun wtf(throwable: Throwable) {}
+  open fun wtf(throwable: Throwable?) {}
 
-  open fun wtf(message: String) {}
+  open fun wtf(message: String?) {}
 
-  open fun wtf(throwable: Throwable, message: String) {}
+  open fun wtf(throwable: Throwable?, message: String?) {}
 }
